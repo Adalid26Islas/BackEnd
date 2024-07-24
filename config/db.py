ef721575948a771dab4c018b7564bdccd1b05589
Adalid26Islas <adalidislasquintero@gmail.com>
2024-07-24 08:15:04 -0600

    first commit

---
+++ config/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://avnadmin:AVNS_m3QJMoNPwGsU2r3_9vo@mysql-c2431c9-adalid-7bc4.e.aivencloud.com:18202/defaultdb'
+#SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Sagitario96@localhost:3306/test'  #Conexión local
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
